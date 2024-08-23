fd4e87fcad0e583fcb4e4ad9033883fdeaf0016b
llucn <llu@deltasoft365.com>
2024-08-23 21:43:07 +0800

    feat: nested generic type

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ console.table(mappedProduct);
 export type AllowString<T> = {
   [P in keyof T]?: T[P] | string
 };
-export let mapperCity: AllowString<City> = { name: "Chengdu", population: 10000000 };
+export let mapperCity: AllowString<City> = { name: "Chengdu", population: "10000000" };
 console.table(mapperCity);
 
 export type MakeReadonly<T> = {
@@ -59,11 +59,51 @@ export type MakeReadonly<T> = {
 export let mapperPerson: MakeReadonly<Person> = { name: "Mian Wang", city: "Zhuji" };
 console.table(mapperPerson);
 
-export let p1: Omit<City, "name" | "population"> = { name: "x" };
+export let p3: Pick<City, "name"> = { name: "Chongqing" };
+console.table(p3);
+
+export let p1: Omit<City, "population"> = { name: "x" };
 console.table(p1);
 
 export type CustomMapped<K extends keyof any, T> = {
   [P in K]: T
 };
-export let p2: CustomMapped<"name" | "age", string> = { name: "AM", age: "10" };
+export let p2: Record<"name" | "age", string> = { name: "AM", age: "10" };
 console.table(p2);
+
+export type resultType<T extends boolean> = T extends true ? string : number;
+export let p4: resultType<true> = "string value";
+let p6: string = p4;
+export let p5: resultType<false> = 1001;
+let p7: number = p5;
+console.table([p4, p5, p6, p7]);
+
+type references = "London" | "Bob" | "Kayak";
+type nestedType<T extends references> = T extends "London" ? City : T extends "Bob" ? Person : Product;
+let p8: nestedType<"London"> = new City("London", 100);
+console.table(p8);
+
+// ---
+type Filter<T, U> = T extends U ? never : T;
+function filterArray<T, U>(data: T[], predicate: (item: any) => item is U): Filter<T, U>[] {
+  return data.filter(e => !predicate(e)) as any;
+}
+const p9 = filterArray(products, (item): item is Person => item instanceof Person);
+console.table(p9);
+
+// ---
+type unionOfPropNames<T, U> = {
+  [P in keyof T]: T[P] extends U ? P : never;
+}[keyof T];
+type A = unionOfPropNames<{ a: number, b: string, c: boolean, d: number }, number>;
+function total<T, P extends unionOfPropNames<T, number>, U extends boolean>(items: T[], propName: P, format: U): resultType<U> {
+  const sum = items.reduce((t, item) => t += Number(item[propName]), 0);
+  return format ? `$${sum.toFixed(2)}` : sum as any;
+}
+const sum: string = total(products, "price", true);
+console.log(`Sum: ${sum}`);
+
+// ---
+
+
+
